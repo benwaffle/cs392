f6387593248df36f4ef9da740a119fac436e256e
Ben Iofel <iofelben@gmail.com>
2016-10-27 17:38:33 -0400

    malloc: part 3
    alloc in gaps, free anywhere

--- src/malloc/malloc.c
+++ src/malloc/malloc.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
 
 typedef struct block {
     struct block *next;
     size_t size;
-    void *mem;
+    void *mem; // is this redundant
 } block;
 
 block *head = NULL;
@@ -29,21 +30,54 @@ void add_node(block *b) {
     }
 }
 
-void remove_last() {
+void remove_node(block *n) {
     if (head == NULL)
         return;
-    block *b;
-    for (b = head; b->next != tail; b = b->next)
-        ;
-    if (b == head) {
+    if (n == head && n == tail) {
         head = tail = NULL;
-    } else {
-        tail = b;
-        tail->next = NULL;
+        return;
     }
+    if (n == head) {
+        head = n->next;
+        return;
+    }
+
+    block *b; // node before n
+    for (b = head; b->next != n; b = b->next)
+        ;
+
+    b->next = n->next;
 }
 
 void *my_malloc(size_t size) {
+    if (head != NULL) {
+        // look for a free gap
+        block *prev = NULL;
+        for (block *b = head; b->next != NULL; b = b->next) {
+            char *endblock = (char*)b->mem + b->size + sizeof(block);
+            char *beginnext = b->next->mem;
+            if (endblock - beginnext >= size + sizeof(block)) {
+                prev = b;
+                break;
+            }
+        }
+
+        // if we have a gap, use ti
+        if (prev != NULL) {
+            printf("Got a gap!\n");
+            void *mem = (char*)prev->mem + prev->size + sizeof(block);
+            *(block*)mem = (block) {
+                .next = prev->next,
+                .size = size,
+                .mem = mem,
+            };
+            prev->next = (block*)mem;
+            return (char*)mem + sizeof(block);
+        }
+    }
+
+    // else increase data segment
+
     void *mem = sbrk(size + sizeof(block));
 
     if (mem == (void*)-1) {
@@ -51,7 +85,7 @@ void *my_malloc(size_t size) {
         exit(1);
     }
 
-    *(block*)mem = (block){
+    *(block*)mem = (block) {
         .size = size,
         .mem = mem,
     };
@@ -65,50 +99,35 @@ void my_free(void *mem) {
     block *b = (void*)((char*)mem - sizeof(block));
     printf("freeing %p, ", b->mem);
 
-    remove_last();
-    void *curbrk = sbrk(0);
-    brk((char*)curbrk - sizeof(block) - b->size);
-    printf("new break = %p\n", sbrk(0));
+    if (b == tail) {
+        void *curbrk = sbrk(0);
+        brk((char*)curbrk - sizeof(block) - b->size);
+        printf("new break = %p\n", sbrk(0));
+    } else {
+        printf("\n");
+    }
+
+    remove_node(b);
 }
 
 int main() {
-    printf("Allocating a bunch of stuff:\n");
-    {
-        void *m = my_malloc(5);
-        printf("\tmalloc -> %p\n", m);
-//        my_free(m);
-    }
+    void *a = my_malloc(5);
+    void *b = my_malloc(5);
+    void *c = my_malloc(5);
+    printf("allocated 3 x 5 bytes\n");
     print_blocks();
-    {
-        void *m = my_malloc(5);
-        printf("\tmalloc -> %p\n", m);
- //       my_free(m);
-    }
-    print_blocks();
-    {
-        void *m = my_malloc(5);
-        printf("\tmalloc -> %p\n", m);
-  //      my_free(m);
-    }
-    print_blocks();
-    {
-        void *m = my_malloc(5);
-        printf("\tmalloc -> %p\n", m);
-        my_free(m);
-    }
-    print_blocks();
-    {
-        void *m = my_malloc(100);
-        printf("\tmalloc -> %p\n", m);
-        my_free(m);
-    }
-    print_blocks();
-    {
-        void *m = my_malloc(5);
-        printf("\tmalloc -> %p\n", m);
-        my_free(m);
-    }
 
-    printf("\nTraversing the list:\n");
+    my_free(b); // free a gap
+    printf("freed middle\n");
+    print_blocks();
+
+    void *d = my_malloc(3);
+    assert(b == d);
+    my_free(d);
+
+    a=a;
+    c=c;
+
+
     print_blocks();
 }
