30fbe68f5018698e74057328d09de532d77dd96a
Ben Iofel <iofelben@gmail.com>
2016-11-03 15:37:02 -0400

    shell: add whicha (a which clone)

---
+++ src/shell/whicha.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+prog=$1
+
+if [[ $# != 1 ]]; then
+    echo "Usage: $0 <program>"
+    exit
+fi
+
+for p in $(echo $PATH | tr ':' ' '); do
+    if [[ -x "$p/$prog" ]]; then
+        echo $p/$prog
+    fi
+done
