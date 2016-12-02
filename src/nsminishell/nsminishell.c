916114cb5edb9f64b748754878f6267099043e18
Ben Iofel <iofelben@gmail.com>
2016-12-02 01:52:25 -0500

    nsminishell: split terminal functions into term.c

--- src/nsminishell/nsminishell.c
+++ src/nsminishell/nsminishell.c
@@ -7,13 +7,11 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <signal.h>
-#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <term.h>
-#include <termios.h>
 #include <unistd.h>
 
 #ifndef CTRL
@@ -26,36 +24,9 @@ bool running = 0;
 struct s_node *history = NULL, *hcur = NULL;
 int retcode = 0;
 
-void rawmode(bool enable)
-{
-    static struct termios old;
-    static bool init = false;
-    if (!init) {
-        init = true;
-        tcgetattr(0, &old);
-    }
-
-    if (enable) {
-        struct termios raw = old;
-
-        raw.c_lflag &= ~(ICANON | IEXTEN); // disable canonical mode
-        raw.c_lflag &= ~(ECHO | ECHONL); // no echo
-        raw.c_lflag &= ~ISIG; // disable signals (INT, QUIT, TSTP, ...)
-
-        raw.c_iflag |= ICRNL; // CR -> NL
-        raw.c_iflag &= ~INLCR; // no NL -> CR
-
-        raw.c_oflag |= OPOST; // implementation-defined output processing
-        raw.c_oflag |= ONLCR; // NL -> CR,NL
-        raw.c_oflag &= ~(OCRNL | ONOCR | ONLRET); // output CR
-
-        raw.c_cc[VMIN] = 1; // minimum number of chars for read()
-        raw.c_cc[VTIME] = 0; // read() timeout
-        tcsetattr(0, TCSANOW, &raw);
-    } else {
-        tcsetattr(0, TCSANOW, &old);
-    }
-}
+void rawmode(bool enable);
+void debug(int line, const char *fmt, ...);
+void clr2eol(int pos);
 
 void shell_prompt()
 {
@@ -132,25 +103,6 @@ void process_command(char *cmd)
     rawmode(true);
 }
 
-void debug(int line, const char *fmt, ...)
-{
-#ifdef DEBUG
-    va_list args;
-    va_start(args, fmt);
-
-    putp(tigetstr("sc")); // save cursor
-    // clang-format off
-    putp(tparm(tigetstr("cup"), tigetnum("lines") - line, 0)); // move cur to y, x
-    // clang-format on
-    putp(tigetstr("dl1")); // delete line
-
-    vprintf(fmt, args);
-    va_end(args);
-
-    putp(tigetstr("rc")); // restore cursor
-#endif
-}
-
 /**
  * insert `len' chars from src into beginning of dst
  */
@@ -158,13 +110,6 @@ void strinsert(char *dst, char *src, int len)
 {
     memmove(dst + len, dst, strlen(dst) + 1); // shift chars
     strncpy(dst, src, len); // copy from src without '\0'
-}
-
-void clr2eol(int pos)
-{
-    if (pos > 0)
-        putp(tparm(tigetstr("cub"), pos)); // move left `pos' spaces
-    putp(tigetstr("el")); // delete to eol
 }
 
 void do_input()
---
+++ src/nsminishell/term.c
@@ -0,0 +1,63 @@
+#define DEBUG
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <term.h>
+#include <termios.h>
+
+void rawmode(bool enable)
+{
+    static struct termios old;
+    static bool init = false;
+    if (!init) {
+        init = true;
+        tcgetattr(0, &old);
+    }
+
+    if (enable) {
+        struct termios raw = old;
+
+        raw.c_lflag &= ~(ICANON | IEXTEN); // disable canonical mode
+        raw.c_lflag &= ~(ECHO | ECHONL); // no echo
+        raw.c_lflag &= ~ISIG; // disable signals (INT, QUIT, TSTP, ...)
+
+        raw.c_iflag |= ICRNL; // CR -> NL
+        raw.c_iflag &= ~INLCR; // no NL -> CR
+
+        raw.c_oflag |= OPOST; // implementation-defined output processing
+        raw.c_oflag |= ONLCR; // NL -> CR,NL
+        raw.c_oflag &= ~(OCRNL | ONOCR | ONLRET); // output CR
+
+        raw.c_cc[VMIN] = 1; // minimum number of chars for read()
+        raw.c_cc[VTIME] = 0; // read() timeout
+        tcsetattr(0, TCSANOW, &raw);
+    } else {
+        tcsetattr(0, TCSANOW, &old);
+    }
+}
+
+void debug(int line, const char *fmt, ...)
+{
+#ifdef DEBUG
+    va_list args;
+    va_start(args, fmt);
+
+    putp(tigetstr("sc")); // save cursor
+    // clang-format off
+    putp(tparm(tigetstr("cup"), tigetnum("lines") - line, 0)); // move cur to y, x
+    // clang-format on
+    putp(tigetstr("dl1")); // delete line
+
+    vprintf(fmt, args);
+    va_end(args);
+
+    putp(tigetstr("rc")); // restore cursor
+#endif
+}
+
+void clr2eol(int pos)
+{
+    if (pos > 0)
+        putp(tparm(tigetstr("cub"), pos)); // move left `pos' spaces
+    putp(tigetstr("el")); // delete to eol
+}
