7b2a8f659aff6864313a5ddfee21308061053a2a
Ben Iofel <iofelben@gmail.com>
2016-12-08 17:02:21 -0500

    http: use a worker process

--- src/http/http.c
+++ src/http/http.c
@@ -1,17 +1,17 @@
 #define _GNU_SOURCE
 #include <arpa/inet.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <sys/sendfile.h>
 #include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // read an arbitrary-length string from fd
 char *read_str(int fd)
@@ -24,8 +24,6 @@ char *read_str(int fd)
     }
 
     while ((ret = read(fd, msg + append_off, oldsize)) == oldsize) {
-        //if (msg[size - 1] == '\n')
-        //    break;
         oldsize = append_off = size;
         msg = realloc(msg, size *= 2);
         if (msg == NULL) {
@@ -42,29 +40,65 @@ char *read_str(int fd)
     return msg;
 }
 
-void sigusr1(int sig) {
+void sigusr1(int sig)
+{
     printf("pausing");
     pause();
     printf("resuming");
 }
 
-void nothing(){}
+void nothing() {}
+
+void worker(int fd)
+{
+    while (true) {
+        char *file = read_str(fd);
+        printf("reading %s\n", file);
+
+        int f = open(file, O_RDONLY);
+        if (f < 0) {
+            printf("no such file\n");
+            size_t len = 0;
+            write(fd, &len, sizeof len);
+        } else {
+            struct stat st;
+            stat(file, &st);
+            size_t len = st.st_size;
+            write(fd, &len, sizeof len);
+            sendfile(fd, f, NULL, len);
+            close(f);
+        }
+    }
+}
 
 int main()
 {
+    int socks[2];
+    socketpair(AF_UNIX, SOCK_STREAM, 0, socks);
+    pid_t child = fork();
+    if (child < 0) {
+        perror("fork");
+        return 1;
+    } else if (child == 0) { // child
+        close(socks[0]);
+        worker(socks[1]);
+        return 0;
+    } else {
+        close(socks[1]);
+    }
+
     sigset_t mask;
     sigfillset(&mask);
     sigdelset(&mask, SIGUSR2); // only allow SIGUSR2
 
-    sigaction(SIGUSR1, &(struct sigaction) {
-        .sa_handler = sigusr1,
-        .sa_mask = mask,
-        .sa_flags = SA_RESTART
-    }, NULL);
+    sigaction(SIGUSR1, &(struct sigaction){.sa_handler = sigusr1,
+                                           .sa_mask = mask,
+                                           .sa_flags = SA_RESTART},
+              NULL);
 
     signal(SIGUSR2, nothing);
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);  
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("socket");
         return 1;
@@ -102,38 +136,36 @@ int main()
             while (*end != ' ')
                 ++end;
             *end = '\0';
-            printf("%s\n", file);
             if (strlen(file) == 0)
                 file = "index.html";
 
             char *fullpath;
             asprintf(&fullpath, "/srv/http/%s", file);
-            int f = open(fullpath, O_RDONLY);
-            if (f < 0) {
-                char msg[] = \
-                    "HTTP/1.1 404 Not Found\r\n" \
-                    "Content-Type: text/html\r\n" \
-                    "\r\n" \
-                    "error 404\r\n";
+            write(socks[0], fullpath, strlen(fullpath) + 1);
+            size_t len;
+            read(socks[0], &len, sizeof len);
+            printf("len = %lu\n", len);
+            if (len == 0) {
+                char msg[] = "HTTP/1.1 404 Not Found\r\n"
+                             "Content-Type: text/html\r\n"
+                             "\r\n"
+                             "error 404\r\n";
                 write(client, msg, sizeof msg);
             } else {
-                char msg[] = \
-                    "HTTP/1.1 200 OK\r\n" \
-                    "Content-Type: text/html\r\n" \
-                    "\r\n";
+                char msg[] = "HTTP/1.1 200 OK\r\n"
+                             "Content-Type: text/html\r\n"
+                             "\r\n";
                 write(client, msg, sizeof msg);
-                struct stat st;
-                stat(fullpath, &st);
-                sendfile(client, f, NULL, st.st_size);
-                close(f);
+                char c[len];
+                read(socks[0], &c, len);
+                write(client, &c, len);
             }
             free(fullpath);
         } else {
-            char msg[] = \
-                "HTTP/1.1 400 Bad Request\r\n" \
-                "Content-Type: text/html\r\n" \
-                "\r\n" \
-                "bad request";
+            char msg[] = "HTTP/1.1 400 Bad Request\r\n"
+                         "Content-Type: text/html\r\n"
+                         "\r\n"
+                         "bad request";
             write(client, msg, sizeof msg);
         }
         free(msg);
